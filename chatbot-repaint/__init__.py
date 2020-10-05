7a8dd7446874fdf6a405e87eb2f83ef8390002bb
Aurum Kathuria <akathuria@rexhomes.com>
2020-10-05 14:34:05 -0700

    reorganize files, and use a subfolder for the main flask app

---
+++ chatbot-repaint/__init__.py
@@ -0,0 +1,25 @@
+import os
+from flask import Flask, request, redirect
+from twilio.twiml.messaging_response import MessagingResponse
+
+app = Flask(__name__)
+
+@app.route('/sms', methods=['GET', 'POST'])
+def sms_reply():
+    """Respond to an incoming text message. """
+    # Start TwiML Response
+    resp = MessagingReponse()
+
+    # Get the user's text message
+    text = request.values.get('body', None)
+
+    # Add message to our response
+    resp.message("Two can play that game!\n" + str(text))
+
+    # Return the message
+    return str(resp)
+
+if __name__ == '__main__':
+    app.run()
+else:
+    application = app
