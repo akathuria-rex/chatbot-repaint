31d7caaac1f19fc92633bb1f3a36a769e483465d
Aurum Kathuria <akathuria@rexhomes.com>
2020-10-05 17:38:08 -0700

    mrenamesimple ditto messsaging flask app, added reply with simply image flask app as default

--- chatbot-repaint/__init__.py
+++ chatbot-repaint/__init__.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, request, redirect
-from twilio.twiml.messaging_response import MessagingResponse
+from twilio.twiml.messaging_response import MessagingResponse, Message
 
 app = Flask(__name__)
 
@@ -8,18 +8,25 @@ app = Flask(__name__)
 def sms_reply():
     """Respond to an incoming text message. """
     # Start TwiML Response
-    resp = MessagingResponse()
+    response = MessagingResponse()
+    message = Message()
 
     # Get the user's text message
     text = request.values.get('body', None)
 
-    # Add message to our response
-    resp.message("Two can play that game!\n" + str(text))
+    # Add text to our message
+    message.body("Two can play that game!\n" + str(text))
+
+    # Add media to our message
+    cool_living_room_image_url = \
+        "https://qa-cdn.rexhomes.com/listing/e69b9c60821e4e5f90d6a2dfcd567644/image.1550854689032.jpg"
+    message.media(cool_living_room_image_url)
+
+    # Add our message to our response
+    response.append(message)
 
     # Return the message
-    return str(resp)
+    return str(response)
 
 if __name__ == '__main__':
     app.run()
-else:
-    application = app
